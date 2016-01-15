59764caa2a2151eb5dc4a70ad4de7451db508b59
Chris Mears <chris.mears@gmail.com>
2016-01-15 12:13:30 -0500

    Update settings

--- settings-dist.php
+++ settings-dist.php
@@ -1,13 +1,20 @@
 <?php
-$date = date("Ymd");                    // Date format for file name
-$file = "db-$date.sql";                 // Backup file name
+
+// MySQL DB settings
 $dbserver = "localhost";                // MySQL database server
 $db = "db_name";                        // MySQL database name
 $user = "user";                         // MySQL database user
 $password = "password";                 // MySQL database password
+
+// Rackspace Settings
 $rackspaceRegion = 'DFW';               // 'DFW',
 $rackspaceUrlType = 'internalURL';      // 'internalURL' or 'publicURL'
 $rackspaceContainer = 'Container-Name'; // Name of the Cloud File container
 $rackspaceUser = "rackspace_user";      // rackspace username
 $rackspaceApiKey = "xxxxxxxxxxxxxxxxx"; // rackspace api key
+
+// Filename settings
+$date = date("Ymd");                    // Date format for file name
+$file = "$db-backup-$date.sql";         // Backup file name
+
 ?>
